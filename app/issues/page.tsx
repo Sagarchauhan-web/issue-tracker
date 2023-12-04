36b557b9a7d32fa9e7a428b419b2324deb795388
Sagar <Sagarchawan4646@gmail.com>
2023-12-04 10:30:30 +0530

    Sort Issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -26,8 +26,17 @@ const IssuesPage = async ({ searchParams }: Props) => {
     ? searchParams.status
     : undefined;
 
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? {
+        [searchParams.orderBy]: 'asc',
+      }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: { status },
+    orderBy: orderBy,
   });
 
   return (
